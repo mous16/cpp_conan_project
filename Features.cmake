0190df37012341cc82f8ed1b27ce53bb1df84534
Francesco Cusolito <mous16@gmail.com>
2023-04-06 12:53:50 +0200

    Switching to setup_project_options and Conan

--- Features.cmake
+++ Features.cmake
@@ -1,11 +1,5 @@
 # building the tests
 option(FEATURE_TESTS "Enable the tests" OFF)
-if(FEATURE_TESTS)
-  list(APPEND VCPKG_MANIFEST_FEATURES "tests")
-endif()
-
-# building the docs
-option(FEATURE_DOCS "Enable the docs" OFF)
 
 # fuzz tests
 option(FEATURE_FUZZ_TESTS "Enable the fuzz tests" OFF)
